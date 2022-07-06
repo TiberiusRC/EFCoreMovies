2daab511988375fa14c5cd05e7b6e5ef88cf073d
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-06 16:16:19 +0200

    Creation of APIs , DTOs and Utilities

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreMovies.DTOs;
+using EFCoreMovies.Entities;
+using EFCoreMovies.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/actors")]
+    public class ActorController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActorController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ActorDTO>> Get(int page = 1, int recordsToTake = 2)
+        {
+            return await context.Actors.AsNoTracking()
+                .OrderBy(g => g.Name)
+                .Select(a => new ActorDTO { Id=a.Id,Name=a.Name,DateOfBirth=a.DateOfBirth})
+                .Paginate(page, recordsToTake)
+                .ToListAsync();
+        }
+    }
+}
---
+++ DTOs/ActorDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreMovies.DTOs
+{
+    public class ActorDTO
+    {
+        //Showable columns in query (filter method)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
