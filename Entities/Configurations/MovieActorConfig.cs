2861b28eefdcd7c0e093bca8e027e15fdb88a944
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-09 17:34:42 +0200

    configuring many to many relation

--- Entities/Configurations/MovieActorConfig.cs
+++ Entities/Configurations/MovieActorConfig.cs
@@ -9,6 +9,10 @@ namespace EFCoreMovies.Entities.Configurations
         {
             //Creation of MovieActor entity
             builder.HasKey(p => new { p.MovieId, p.ActorId });
+
+            // configuring many to many relation by adding multiple one to may relations without Skipping
+            builder.HasOne(p => p.Actor).WithMany(p => p.MovieActors).HasForeignKey(p => p.ActorId);
+            builder.HasOne(p => p.Movie).WithMany(p => p.MovieActors).HasForeignKey(p => p.MovieId);
         }
     }
 }
