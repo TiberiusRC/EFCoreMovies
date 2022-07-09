3fb36b89cf34e6cee45eafc513f9c32ea43bd2a8
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-09 17:41:30 +0200

    example of many to many relations with skipping Navigation

--- Entities/Configurations/MovieConfig.cs
+++ Entities/Configurations/MovieConfig.cs
@@ -10,6 +10,13 @@ namespace EFCoreMovies.Entities.Configurations
             //Creation of Movie entity
             builder.Property(p => p.Title).HasMaxLength(250).IsRequired();
             builder.Property(p => p.PosterUrl).HasMaxLength(500).IsUnicode(false);
+
+            //Many to many relation with skipping navigation
+            builder.HasMany(p => p.Genres).WithMany(p => p.Movies);
+                //.UsingEntity(j => j.ToTable("GenreMovies").HasData(new {MoviesId =1,GenresId=7}));// Creation of a intermediate table..(HasData is to seed the table-optional)
+
+
+
         }
     }
 }
